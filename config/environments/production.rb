04483f91ff83ca3334394c8024a3ea66c28ea32f
Bridget Almas <balmas@balmas-ThinkPad-X1.(none)>
2013-06-12 11:04:42 -0400

    setting assets.debug=true because without it jquery handlers aren't working?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ BlacklightTest::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.debug = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
