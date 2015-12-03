1afa4e3e345909abea74204de49c2430c95aa2eb
Bridget Almas <balmas@gmail.com>
2015-12-03 08:36:41 -0500

    make sure to have latest data

--- catalog-update.sh
+++ catalog-update.sh
@@ -3,7 +3,17 @@
 
 # Prerequisite: Setup via catalog-update.setup.sh
 
-# 1. IMPORT DATA
+# 1. UPDATE LOCAL GIT REPOS
+
+cd ~/catalog_pending
+git checkout master
+git pull
+
+cd ~/catalog_data
+git checkout master
+git pull
+
+# 2. IMPORT/UPDATE CITE_COLLECTION DATA FROM GIT REPOS
 cd ~/cite_collections_rails
 
 # IF FIRST LOAD OF DATA THEN
@@ -12,20 +22,20 @@ rake catalog_pending_import type="all"
 rake catalog_pending_import type="latest" 
 
 
-# 2. BUILD ATOM FEEDS
+# 3. BUILD ATOM FEEDS
 # IF FIRST LOAD OF DATA THEN
 rake build_atom_feed type="all"
 # OTHERWISE IF UPDATE
 rake build_atom_feed type="latest"
 
-# 3. IMPORT ATOM FEEDS
+# 4. IMPORT ATOM FEEDS
 cd ~/perseus_catalog
 bundle exec rake parse_records file_type='atom' rec_file='/home/ubuntu/FRBR.feeds.YYYYMMDD'
 
-# 4. IMPORT DATA TO SOLR
+# 5. IMPORT DATA TO SOLR
 curl http://localhost:8080/solr-4.5.1/db/dataimport?command=full-import&clean=false
 
-# 5. DEPLOY TO PRODUCTION
+# 6. DEPLOY TO PRODUCTION
 # dump mysql
 # reload on catalog0.perseus.tufts.edu
 # rerun solr import on catalog0.perseus.tufts.edu
